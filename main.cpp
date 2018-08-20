3f293a2516fbfb151a4452dc99dfaba811b3956c
nooooooname <2992865700@qq.com>
2018-08-20 21:09:15 +0800

    更改输出字符串和输出函数

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "myheader.h"
 int main(int argc, char *argv[])
 {
-	char str[] = "Hello git\n";
-	printf(str);
+	char str[] = "Hello git";
+	puts(str);
 	return 0;
 }
---
+++ myheader.h
@@ -0,0 +1,1 @@
+#define NULLL 0
